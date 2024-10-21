467979d10b970c132aad035f198bc9013c9c4507
Samuli puhto <samuli.puhto@gmail.com>
2024-10-21 15:26:51 +0300

    alku

---
+++ ika_ohjelma/src/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int ika = 10;
+
+        if (ika >= 0 && ika < 18)
+        {
+            System.out.println("Olet alaikäinen");
+        }
+    }
+}
